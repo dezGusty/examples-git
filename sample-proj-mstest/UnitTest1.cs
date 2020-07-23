9e1d75e5b477e1d937ba715440f8b8677fe4d53c
Augustin Preda <augustin.preda@siemens.com>
2020-07-23 18:03:23 +0300

    Add sample unit-test-project

---
+++ sample-proj-mstest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sample_proj_mstest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
