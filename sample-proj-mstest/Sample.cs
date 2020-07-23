ec2230401c93109e7f06c5558309de89c70a4a60
Augustin Preda <augustin.preda@siemens.com>
2020-07-23 18:29:46 +0300

    Add some unit tests

---
+++ sample-proj-mstest/Sample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace sample_proj_mstest
+{
+    internal class Sample
+    {
+        public static double GetFirstRepetitionInterval(
+            DateTime appLaunchTime,
+            DateTime desiredStartTime,
+            double frequencyInMs)
+        {
+            // Set the first timer occurrence. Differs from the typical duration afterwards.
+            // E.g. daily timer, for 11:00. If the application is started at 09:00, the first
+            // time interval should be set for 2 hours. Subsequent intervals would be set to 24hrs.
+            if (appLaunchTime.CompareTo(desiredStartTime) < 0)
+            {
+                // desiredStartTime is in the FUTURE from appLaunchTime.
+                // E.g. If Now is 09:00 and SyncStartTime is at 11:00. 24hrs standard repetition.
+                // => interval should be 11-9 => 2 hrs
+                double result = desiredStartTime.Subtract(appLaunchTime).TotalMilliseconds;
+                return result;
+            }
+            else
+            {
+                // desiredStartTime is in the PAST from appLaunchTime.
+                // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+                // => initial interval should be 24     +11 -13 => 22hrs
+                // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+                // => initial interval should be 6      +11 -13 => 4hrs
+                // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+                // => initial interval should be 1.5*2  +11 -13 => 1hrs7
+                //             frequencyInMs + desiredStartTime - appLaunchTime
+                // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 0.5hrs standard repetition.
+                // => initial interval should be 0.5*5  +11 -13 => 0.5 hrs (cannot be now)
+                //             frequencyInMs + desiredStartTime - appLaunchTime
+                int multiplier = (int)(appLaunchTime.Subtract(desiredStartTime).TotalMilliseconds / 1000)
+                    / (int)(frequencyInMs / 1000);
+                double result = frequencyInMs * (multiplier + 1)
+                        + desiredStartTime.Subtract(appLaunchTime).TotalMilliseconds;
+
+                return result;
+            }
+        }
+    }
+}
--- sample-proj-mstest/UnitTest1.cs
+++ sample-proj-mstest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace sample_proj_mstest
 {
@@ -8,6 +9,94 @@ namespace sample_proj_mstest
         [TestMethod]
         public void TestMethod1()
         {
+            // SyncStartTime is in the PAST.
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+            // => initial interval should be 24     +11 -13 => 22hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+            // => initial interval should be 6      +11 -13 => 4hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+            // => initial interval should be 1.5*2  +11 -13 => 1hrs
+
+            DateTime appStartTime = new DateTime(2020, 01, 24, 13, 0, 0);
+            DateTime desiredSyncTime = new DateTime(2020, 01, 24, 11, 0, 0);
+            double syncFreq = 24 * 60 * 60 * 1000;
+            double expectedMillisTillThen = 22 * 60 * 60 * 1000;
+
+            Assert.AreEqual(
+                expectedMillisTillThen,
+                Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
+                1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // SyncStartTime is in the PAST.
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+            // => initial interval should be 24     +11 -13 => 22hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+            // => initial interval should be 6      +11 -13 => 4hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+            // => initial interval should be 1.5*2  +11 -13 => 1hrs
+
+            DateTime appStartTime = new DateTime(2020, 01, 24, 13, 0, 0);
+            DateTime desiredSyncTime = new DateTime(2020, 01, 24, 11, 0, 0);
+            double expectedMillisTillThen = 4 * 60 * 60 * 1000;
+            double syncFreq = 6 * 60 * 60 * 1000;
+
+            Assert.AreEqual(
+                expectedMillisTillThen,
+                Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
+                1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // SyncStartTime is in the PAST.
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+            // => initial interval should be 24     +11 -13 => 22hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+            // => initial interval should be 6      +11 -13 => 4hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+            // => initial interval should be 1.5*2  +11 -13 => 1hrs
+
+            DateTime appStartTime = new DateTime(2020, 01, 24, 13, 0, 0);
+            DateTime desiredSyncTime = new DateTime(2020, 01, 24, 11, 0, 0);
+            double expectedMillisTillThen = 0.5 * 60 * 60 * 1000;
+            double syncFreq = 0.5 * 60 * 60 * 1000;
+
+            Assert.AreEqual(
+                expectedMillisTillThen,
+                Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
+                1);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // SyncStartTime is in the PAST.
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+            // => initial interval should be 24     +11 -13 => 22hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+            // => initial interval should be 6      +11 -13 => 4hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+            // => initial interval should be 1.5*2  +11 -13 => 1hrs
+
+            // 10 minute sync freq.
+            // start app at 19:12
+            // set sync time for 18:14
+            // => should start in 2 minutes (at 19:14).
+            DateTime appStartTime = new DateTime(2020, 01, 24, 19, 12, 0);
+            DateTime desiredSyncTime = new DateTime(2020, 01, 24, 18, 14, 0);
+            double expectedMillisTillThen = 2 * 60 * 1000;
+            double syncFreq = 10 * 60 * 1000;
+
+            Assert.AreEqual(
+                expectedMillisTillThen,
+                Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
+                1);
         }
     }
 }
