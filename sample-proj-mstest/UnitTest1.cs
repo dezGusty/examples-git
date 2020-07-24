7cd53dfe92550f90a5c0cc1cf013544e5c71bb61
Augustin Preda <augustin.preda@siemens.com>
2020-07-24 09:49:59 +0300

    Add another unit test

--- sample-proj-mstest/UnitTest1.cs
+++ sample-proj-mstest/UnitTest1.cs
@@ -98,5 +98,31 @@ namespace sample_proj_mstest
                 Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
                 1);
         }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // SyncStartTime is in the PAST.
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 24hrs standard sync freq.
+            // => initial interval should be 24     +11 -13 => 22hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 6hrs standard repetition.
+            // => initial interval should be 6      +11 -13 => 4hrs
+            // E.g. If Now is 13:00 and SyncStartTime is at 11:00. 1.5hrs standard repetition.
+            // => initial interval should be 1.5*2  +11 -13 => 1hrs
+
+            // 10 minute sync freq.
+            // start app at 19:12
+            // set sync time for 21:14
+            // => should start in 122 minutes (at 21:14).
+            DateTime appStartTime = new DateTime(2020, 01, 24, 19, 12, 0);
+            DateTime desiredSyncTime = new DateTime(2020, 01, 24, 21, 14, 0);
+            double expectedMillisTillThen = 122 * 60 * 1000;
+            double syncFreq = 10 * 60 * 1000;
+
+            Assert.AreEqual(
+                expectedMillisTillThen,
+                Sample.GetFirstRepetitionInterval(appStartTime, desiredSyncTime, syncFreq),
+                1);
+        }
     }
 }
