f991a208c81fb7ef0381cc1076da0b89b3099ee2
Augustin Preda <augustin.preda@siemens.com>
2022-07-14 12:35:13 +0300

    replaced spacing with 2 spaces indent

--- formatting/list_files.rs
+++ formatting/list_files.rs
@@ -2,12 +2,12 @@ use std::error::Error;
 use std::path::Path;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let dir_path = ".";
-    let path = Path::new(dir_path);
-    for file_res in path.read_dir()? {
-        let file = file_res?;
-        println!("File: {}", file.file_name().to_string_lossy());
-    }
+  let dir_path = ".";
+  let path = Path::new(dir_path);
+  for file_res in path.read_dir()? {
+    let file = file_res?;
+    println!("File: {}", file.file_name().to_string_lossy());
+  }
 
-    Ok(())
+  Ok(())
 }
